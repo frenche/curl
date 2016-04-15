276b619e8825db5154c09a2b044f0a6c6448b163
Isaac Boukris <iboukris@gmail.com>
2016-04-15 14:47:58 +0300

    what

--- lib/url.c
+++ lib/url.c
@@ -5549,7 +5549,7 @@ static CURLcode create_conn(struct SessionHandle *data,
 
 #ifndef CURL_DISABLE_PROXY
   /*************************************************************
-   * Extract the user and password from the authentication string
+   * xxExtract the user and password from the authentication string
    *************************************************************/
   if(conn->bits.proxy_user_passwd) {
     result = parse_proxy_auth(data, conn);
