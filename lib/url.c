frenche/curl

commit 0b8d682f81ee9acb763dd4c9ad805fe08d1227c0

Don't mix unix domain sockets with regular ones
When reusing a connection, make sure the unix domain
socket option matches.

lib/url.c
@@ -2800,6 +2800,10 @@ static void conn_free(struct connectdata *conn)
   Curl_safefree(conn->localdev);
   Curl_free_ssl_config(&conn->ssl_config);
 
+#ifdef USE_UNIX_SOCKETS
+  Curl_safefree(conn->unix_domain_socket);
+#endif
+
   free(conn); /* free all the connection oriented data */
 }
 
@@ -3328,6 +3332,17 @@ ConnectionExists(struct Curl_easy *data,
           continue;
         }
       }
+
+#ifdef USE_UNIX_SOCKETS
+      if(needle->unix_domain_socket) {
+        if(!check->unix_domain_socket)
+          continue;
+        if(strcmp(needle->unix_domain_socket, check->unix_domain_socket))
+          continue;
+      }
+      else if(check->unix_domain_socket)
+        continue;
+#endif
 
       if((needle->handler->flags&PROTOPT_SSL) !=
          (check->handler->flags&PROTOPT_SSL))
@@ -5539,11 +5554,11 @@ static CURLcode resolve_server(struct Curl_easy *data,
     struct Curl_dns_entry *hostaddr;
 
 #ifdef USE_UNIX_SOCKETS
-    if(data->set.str[STRING_UNIX_SOCKET_PATH]) {
+    if(conn->unix_domain_socket) {
       /* Unix domain sockets are local. The host gets ignored, just use the
        * specified domain socket address. Do not cache "DNS entries". There is
        * no DNS involved and we already have the filesystem path available */
-      const char *path = data->set.str[STRING_UNIX_SOCKET_PATH];
+      const char *path = conn->unix_domain_socket;
 
       hostaddr = calloc(1, sizeof(struct Curl_dns_entry));
       if(!hostaddr)
@@ -5694,6 +5709,10 @@ static void reuse_conn(struct connectdata *old_conn,
   old_conn->recv_pipe = NULL;
 
   Curl_safefree(old_conn->master_buffer);
+
+#ifdef USE_UNIX_SOCKETS
+  Curl_safefree(old_conn->unix_domain_socket);
+#endif
 }
 
 /**
@@ -5900,9 +5919,16 @@ static CURLcode create_conn(struct Curl_easy *data,
     proxy = detect_proxy(conn);
 
 #ifdef USE_UNIX_SOCKETS
-  if(proxy && data->set.str[STRING_UNIX_SOCKET_PATH]) {
-    free(proxy);  /* Unix domain sockets cannot be proxied, so disable it */
-    proxy = NULL;
+  if(data->set.str[STRING_UNIX_SOCKET_PATH]) {
+    if(proxy) {
+      free(proxy); /* Unix domain sockets cannot be proxied, so disable it */
+      proxy = NULL;
+    }
+    conn->unix_domain_socket = strdup(data->set.str[STRING_UNIX_SOCKET_PATH]);
+    if(conn->unix_domain_socket == NULL) {
+      result = CURLE_OUT_OF_MEMORY;
+      goto out;
+    }
   }
 #endif
 

lib/urldata.h
@@ -1099,6 +1099,10 @@ struct connectdata {
   struct connectbundle *bundle; /* The bundle we are member of */
 
   int negnpn; /* APLN or NPN TLS negotiated protocol, CURL_HTTP_VERSION* */
+
+#ifdef USE_UNIX_SOCKETS
+  char *unix_domain_socket;
+#endif
 };
 
 /* The end of connectdata. */

commit b7e989788782b434f2187bc0fc2c864f956093d6

unix-socket: fix proxy support
Fix unix-socket support for proxy-disbaled builds.
Regression introduced in:
0b8d682f81ee9acb763dd4c9ad805fe08d1227c0

In addition, fix unix-socket to not ignore proxy
when set, but instead to connect to the proxy host
via the unix-socket.

Signed-off-by: Isaac Boukris <iboukris@gmail.com>
Figured-out-by: Jay Satiro
Reported-by: mccormickt12

Fixes: #1274

docs/libcurl/opts/CURLOPT_ABSTRACT_UNIX_SOCKET.3
@@ -29,10 +29,10 @@ CURLOPT_ABSTRACT_UNIX_SOCKET \- set an abstract Unix domain socket
 CURLcode curl_easy_setopt(CURL *handle, CURLOPT_ABSTRACT_UNIX_SOCKET, char *path);
 .SH DESCRIPTION
 Enables the use of an abstract Unix domain socket instead of establishing a TCP
-connection to a host. The parameter should be a char * to a zero terminated string
-holding the path of the socket. The path will be set to \fIpath\fP prefixed by a
-NULL byte (this is the convention for abstract sockets, however it should be stressed
-that the path passed to this function should not contain a leading NULL).
+connection to a host or to a proxy. The parameter should be a char * to a zero
+terminated string holding the path of the socket. The path will be set to \fIpath\fP
+prefixed by a NULL byte (this is the convention for abstract sockets, however it should
+be stressed that the path passed to this function should not contain a leading NULL).
 
 On non-supporting platforms, the abstract address will be interpreted as an empty
 string and fail gracefully, generating a run-time error.

docs/libcurl/opts/CURLOPT_UNIX_SOCKET_PATH.3
@@ -34,18 +34,15 @@ empty string will result in an error at some point, it will not disable use of
 Unix domain sockets.
 
 When enabled, curl will connect to the Unix domain socket instead of
-establishing a TCP connection to a host. Since no TCP connection is created,
-curl does not need to resolve the DNS hostname in the URL.
+establishing a TCP connection to a host or to a proxy. Since no TCP connection
+is created, curl does not need to resolve the DNS hostname in the URL.
 
 The maximum path length on Cygwin, Linux and Solaris is 107. On other platforms
 it might be even less.
 
-Proxy and TCP options such as
+TCP option
 .BR CURLOPT_TCP_NODELAY "(3)
-are not supported. Proxy options such as
-.BR CURLOPT_PROXY "(3)
-have no effect either as these are TCP-oriented, and asking a proxy server to
-connect to a certain Unix domain socket is not possible.
+is ignored.
 
 The application does not have to keep the string around after setting this
 option.

lib/url.c
@@ -6252,6 +6252,17 @@ static CURLcode create_conn(struct Curl_easy *data,
     }
   }
 
+#ifdef USE_UNIX_SOCKETS
+  if(data->set.str[STRING_UNIX_SOCKET_PATH]) {
+    conn->unix_domain_socket = strdup(data->set.str[STRING_UNIX_SOCKET_PATH]);
+    if(conn->unix_domain_socket == NULL) {
+      result = CURLE_OUT_OF_MEMORY;
+      goto out;
+    }
+    conn->abstract_unix_socket = data->set.abstract_unix_socket;
+  }
+#endif
+
 #ifndef CURL_DISABLE_PROXY
   /*************************************************************
    * Extract the user and password from the authentication string
@@ -6304,21 +6315,6 @@ static CURLcode create_conn(struct Curl_easy *data,
 #endif /* CURL_DISABLE_HTTP */
 
   Curl_safefree(no_proxy);
-
-#ifdef USE_UNIX_SOCKETS
-  if(data->set.str[STRING_UNIX_SOCKET_PATH]) {
-    if(proxy) {
-      free(proxy); /* Unix domain sockets cannot be proxied, so disable it */
-      proxy = NULL;
-    }
-    conn->unix_domain_socket = strdup(data->set.str[STRING_UNIX_SOCKET_PATH]);
-    if(conn->unix_domain_socket == NULL) {
-      result = CURLE_OUT_OF_MEMORY;
-      goto out;
-    }
-    conn->abstract_unix_socket = data->set.abstract_unix_socket;
-  }
-#endif
 
   if(proxy && (!*proxy || (conn->handler->flags & PROTOPT_NONETWORK))) {
     free(proxy);  /* Don't bother with an empty proxy string or if the